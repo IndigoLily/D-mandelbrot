9f09abc2ce391f202d19e281552ddfca6dea3138
Indigo Lily <yulex.42@gmail.com>
2018-01-01 06:06:07 -0400

    Takes command line arguments

--- mandelbrot.d
+++ mandelbrot.d
@@ -2,16 +2,27 @@ import std.complex;
 import std.stdio;
 import std.conv;
 import std.math;
+import std.getopt;
 
 void main(string[] args) {
-    immutable uint MAX = 1000;
+    uint max = 1000;
     uint width  = 400;
     uint height = 400;
+    real zoom = 1;
+    ubyte aa = 1;
+    real posR = 0;
+    real posI = 0;
+
+    getopt(args,
+            "iterations|itr", &max,
+            "width|w", &width,
+            "height|h", &height,
+            "zoom|z", &zoom,
+            "aa|a", &aa,
+            "x|r", &posR,
+            "y|i", &posI);
+
     uint small  = (width < height) ? width : height;
-    real zoom = 512;
-    immutable ubyte AA = 1;
-    real posR = -0.757843017578125;
-    real posI =  0.073272705078125;
 
     ubyte[] imageData;
     foreach (y; 0 .. height) {
@@ -20,19 +31,19 @@ void main(string[] args) {
             real sumG = 0;
             real sumB = 0;
 
-            foreach (xAA; 0 .. AA) {
-                foreach (yAA; 0 .. AA) {
+            foreach (xaa; 0 .. aa) {
+                foreach (yaa; 0 .. aa) {
                     Complex!real c;
-                    c.re = (x + real(xAA) / AA - width  / 2) / small * 4 / zoom + posR;
-                    c.im = (y + real(yAA) / AA - height / 2) / small * 4 / zoom + posI;
+                    c.re = (x + real(xaa) / aa - width  / 2) / small * 4 / zoom + posR;
+                    c.im = (y + real(yaa) / aa - height / 2) / small * 4 / zoom + posI;
                     Complex!real z = 0;
 
                     uint i = 0;
-                    for (; i < MAX && z.re * z.re + z.im * z.im < 4; ++i) {
+                    for (; i < max && z.re * z.re + z.im * z.im < 4; ++i) {
                         z = z * z + c;
                     }
 
-                    if (i != MAX) {
+                    if (i != max) {
                         sumR += (1 + cos(i * PI / 110.0L)) / 2 * 0xFF;
                         sumG += (1 + cos(i * PI / 120.0L)) / 2 * 0xFF;
                         sumB += (1 + cos(i * PI / 130.0L)) / 2 * 0xFF;
@@ -40,9 +51,9 @@ void main(string[] args) {
                 }
             }
 
-            imageData ~= cast(ubyte)(sumR / AA / AA);
-            imageData ~= cast(ubyte)(sumG / AA / AA);
-            imageData ~= cast(ubyte)(sumB / AA / AA);
+            imageData ~= cast(ubyte)(sumR / aa / aa);
+            imageData ~= cast(ubyte)(sumG / aa / aa);
+            imageData ~= cast(ubyte)(sumB / aa / aa);
         }
 
         writeln("Render: ", y.to!real / height * 100, r"% done");
